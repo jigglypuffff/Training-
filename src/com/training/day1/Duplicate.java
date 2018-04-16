efaa234f06dbd8d6696a627570aa712bd5c31402
trainee <aishwaryasawant.2904@gmai.com>
2018-04-16 17:56:16 +0530

    Committed successfully !

---
+++ src/com/training/day1/Duplicate.java
@@ -0,0 +1,41 @@
+package com.training.day1;
+
+import java.util.Scanner;
+
+public class Duplicate 
+{
+	 static Scanner s;
+	        public static void main(String [] args) 
+	        {
+	            String n, a[];
+	            int i,j,l,count=0;
+	            System.out.println("Enter the word or integer : ");
+	            s= new Scanner(System.in);
+	            n=s.nextLine();
+	            a=n.split("");
+	            l = a.length;
+	            
+	            for(i=0; i<l; i++)
+	            {
+	                for(j=i+1; j<l; j++)
+	                {
+	                    if(a[i].equals(a[j])) 
+	                    {
+	                    	count++;
+	                       
+	                    }
+	                }
+	            }
+	            
+	            if(count>=1)
+	            {
+	            	System.out.println("True");
+                
+	            }
+	            else
+	            {
+            	  System.out.println("False");
+	            }
+	       }
+}
+
