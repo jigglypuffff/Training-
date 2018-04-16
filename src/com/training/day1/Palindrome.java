246d01a2839b103f391a2663a8f7ee6133690340
trainee <aishwaryasawant.2904@gmai.com>
2018-04-16 16:40:10 +0530

    Second commit modified !

---
+++ src/com/training/day1/Palindrome.java
@@ -0,0 +1,49 @@
+package com.training.day1;
+
+
+import java.io.IOException;
+import java.util.*;
+
+public class Palindrome
+{
+   //function to check number is Palindrome or not
+   public static boolean isPalindrome(int num)
+   {
+       int sum,tNum;
+       //find reverse number
+       tNum=num;
+       sum =0;
+       while(tNum>0)
+       {
+           sum = (sum*10) + (tNum%10);
+           tNum/=10;
+       }
+        
+       if(num==sum)
+           return true;
+       else
+           return false;
+   }
+    
+   public static void main(String args[]) throws IOException, InputMismatchException
+   {
+       int num;
+        
+       Scanner bf=new Scanner(System.in);
+        
+       //input an integer number
+       System.out.print("Enter any integer number: ");
+       num= bf.nextInt();
+             try {
+            	  if(isPalindrome(num))
+            		  System.out.println(num + " is a Palindrome Number.");
+            	  else
+            		  System.out.println(num + " is not a Palindrome Number.");
+            
+ }
+             catch(Exception e)
+             {
+            	 System.out.println("Enter only positive and negative integers.");
+             }
+}
+}
---
+++ src/com/training/day1/Reverse.java
@@ -0,0 +1,28 @@
+package com.training.day1;
+import java.util.*;
+class Reverse
+{
+public static void main(String[] aaa)
+{
+String s;
+String rev="";
+String temp=" ";
+Scanner ob=new Scanner(System.in);
+System.out.println(" enter String");
+s=ob.nextLine();
+for(int i=s.length()-1;i>=0;i--)
+    rev=rev+s.charAt(i);
+//System.out.println(rev);
+String[] a=rev.split(" ");
+int j=a.length-1;
+while(j>=0)
+{
+    temp=temp+a[j];
+
+    temp=temp+" ";
+    j--;
+
+}
+System.out.println(temp);
+}
+}
