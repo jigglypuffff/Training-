51ac15aada7f473ff0ffa39d06f670649bb680ed
jigglypuffff <aishwaryasawant.2904@gmail.com>
2018-04-20 10:23:01 +0530

    Day 4 commit

--- src/com/training/Day4/src/com/training/day4/ArrayMix.java
+++ src/com/training/Day4/src/com/training/day4/ArrayMix.java
@@ -2,12 +2,12 @@ package com.training.day4;
 
 public class ArrayMix {
 
-	public static <E> void printArray(E[] array) {
-		for (E element : array)
-			System.out.println(element);
+	public static <E> void printArray(final E[] array) {
+		for (final E elem : array)
+			System.out.println(elem);
 	}
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		printArray(new Integer[] { 1, 2, 3 });
 		printArray(new String[] { "Hello", "World" });
 	}
