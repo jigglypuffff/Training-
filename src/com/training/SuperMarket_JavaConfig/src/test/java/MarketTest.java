7fc9caeaa42b64a7161c66e96708e8797ab6cfbd
jigglypuffff <aishwaryasawant.2904@gmail.com>
2018-05-02 18:11:22 +0530

    Updated

--- src/com/training/SuperMarket_JavaConfig/src/test/java/MarketTest.java
+++ src/com/training/SuperMarket_JavaConfig/src/test/java/MarketTest.java
@@ -1,58 +1,89 @@
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.cg.market.controller.ApplicationConfig;
 import com.cg.market.model.Customer;
 import com.cg.market.model.Goods;
-import com.cg.market.model.Retailer;
 import com.cg.market.model.Supplier;
+import com.cg.market.service.CustomerService;
+import com.cg.market.service.GoodsService;
+import com.cg.market.service.RetailerService;
+import com.cg.market.service.SupplierService;
 
 public class MarketTest {
 
+	ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
 
-		Customer customer=new Customer(1,"aishwarya","London","cash");
-		
-		Supplier supplier=new Supplier(101,"ashish","mumbai",2,111,200.2);
-		
-		Goods goods=new Goods(101,"TV","1",1000);
-		
-		Retailer retailer=new Retailer(1,"Yash","Delhi");
-		
-		
 
-		@Test
-		public void customerTest() {
-				assertEquals(1,customer.getCustomerId());
-				assertEquals("aishwarya",customer.getCustomerName());
-				assertEquals("London",customer.getCustomerAddress());
-				assertEquals("cash",customer.getPaymentMode());
-		}
-		@Test
-		public void goodsTest() {
-				assertEquals(101,goods.getGoodsId());
-				assertEquals("TV",goods.getGoodsName());
-				assertEquals("1",goods.getGoodsQuantity());
-			
-		}
-		@Test
-		public void supplierTest() {
-				assertEquals(101,supplier.getSupplierId());
-				assertEquals("ashish",supplier.getSupplierName());
-				assertEquals("mumbai",supplier.getSupplierAddress());
-				assertEquals(2,supplier.getQuantityOrder());
-				assertEquals(111,supplier.getOrderId());
-		}
-				//assertEquals("200.2",supplier.getAmount());
+	CustomerService custService = (CustomerService) context.getBean("customerService");
+	GoodsService goodsService = (GoodsService) context.getBean("goodsService");
+	SupplierService supplierService = (SupplierService) context.getBean("supplierService");
+	RetailerService retailerService = (RetailerService) context.getBean("retailerService");
 
-		
-				/*@Test
-				public void retailerTest() {
-						
-					assertEquals(1,retailer.getRetailerName());
-						assertEquals("Yash",retailer.getRetailerName());
-						assertEquals("Delhi",retailer.getRetailerAddress());
-		
-		
-		}*/
 
+
+@Test
+public void customerTest() {
+
+	Customer customer = new Customer(1, "aishwarya", "London", "cash");
+	
+	Customer value=custService.addCustomer(customer);
+	assertEquals(customer, value);
 }
+
+@Test
+public void goodsTest() {
+
+	Goods goods = new Goods(102, "tv", "2",2000);
+
+	Goods value=goodsService.addGoods(goods);
+	assertEquals(goods, value);
+}
+
+
+@Test
+public void supplierTest() {
+
+	Supplier sup = new Supplier(1000, "Ashish Sharma", "Delhi", 4 ,1,3000);
+	
+	Supplier value=supplierService.addSupplier(sup);
+	assertEquals(sup, value);
+}
+
+
+@Test
+public void customerRemoveTest() {
+
+	Customer customer = new Customer(1, "aishwarya", "London", "cash");
+//	Customer customer = new Customer(2, "Yash", "New York", "cash");
+	
+	Customer value=custService.removeCustomer(customer);
+	assertEquals(customer, value);
+}
+
+
+@Test
+public void goodsRemoveTest() {
+
+	Goods goods = new Goods(102, "Fridge", "2",1212);
+
+	Goods value=goodsService.removeGoods(goods);
+	assertEquals(goods, value);
+}
+@Test
+public void supplierRemoveTest() {
+
+	Supplier sup = new Supplier(1, "shruti", "pune", 2,1,200);
+	//	CustomerDao customerService;
+	Supplier value=supplierService.removeSupplier(sup);
+	assertEquals(sup, value);
+}
+
+	
+	
+	}
+
+
